727c0b5bd5b3aa36da0bd4f05239d9cee9b1f4a7
muroni-cristiano <cristiano.muroni@compasso.com.br>
2021-06-29 09:54:23 -0300

    subindo

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#**API Gateway Take-Git**<h1>
+##Está é uma API que consome a API do GitHub da Take Blip.<h2>
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  try {
+    let { data } = await axios("https://api.github.com/users/takenet/repos");
+    let tongue = "C#";
+    let result = data.filter((item) => {
+      return item.language == tongue;
+    });
+    result.sort(function (a, b) {
+      return a.created_at - b.created_at;
+    });
+    return res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ message: "erro, não foi possível executar esse serviço" });
+  }
+});
+
+app.listen(port, () => console.log(`Server running at port ${port}`));
